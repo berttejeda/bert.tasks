1be4cd83c3644f21d8414a4465456e9e4f48caff
Bert Tejeda <berttejeda@gmail.com>
2024-11-13 23:33:43 -0500

    Expand path for playbook arg, localize scope for kingpin app object

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/berttejeda/bert.yamlcli/ansible"
 	logger "github.com/sirupsen/logrus"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -12,12 +13,26 @@ func main() {
 	// First positional parameter is the path to the playbook
 	var playbook string = os.Args[1]
 	if strings.HasSuffix(playbook, ".yaml") || strings.HasSuffix(playbook, ".yml") {
-  	// Remove the first positional parameter by re-slicing os.Args
-  	os.Args = append(os.Args[:1], os.Args[2:]...)
+		// Remove the first positional parameter by re-slicing os.Args
+		if strings.HasPrefix(playbook, "~/") {
+			dirname, _ := os.UserHomeDir()
+			playbook = filepath.Join(dirname, playbook[2:])
+		}
+		os.Args = append(os.Args[:1], os.Args[2:]...)
 	} else {
 		playbook = "Taskfile.yaml"
 	}
-	cli, cliArguments := ansible.MakeCLIFromAnsiblePlaybook(playbook)
-	logger.Debug(cliArguments)
-	fmt.Println(cli)
+	// Create the application
+	app := kingpin.New("", "")
+	debug := app.Flag("debug", "Enable Debug Logging").Bool()
+	cli, cliArguments := ansible.MakeCLIFromAnsiblePlaybook(app, playbook)
+	if *debug {
+		logger.SetLevel(logger.DebugLevel)
+	} else {
+		logger.SetLevel(logger.InfoLevel)
+	}
+	logger.Debug(*debug)
+	logger.Debug(*cliArguments["foo"].(*string))
+	logger.Debug(*cliArguments["bar"].(*string))
+	logger.Debug(cli)
 }
